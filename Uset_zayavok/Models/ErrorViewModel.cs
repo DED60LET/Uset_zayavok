bcc1141cddada9d86ee197b0018d02fbb088069a
user <user@LAPTOP-N77DAAQ8>
2026-02-25 20:20:05 +0500

    Добавьте файлы проекта.

---
+++ Uset_zayavok/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Uset_zayavok.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

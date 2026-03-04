312d3a9f668505698c4b26b8e928da271d4026e7
DED60LET <slavakuriva@gmail.com>
2026-03-04 22:04:05 +0500

    Жлбавление страницы входа и ограничение

--- Uset_zayavok/Controllers/HomeController.cs
+++ Uset_zayavok/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Uset_zayavok.Models;
 
 namespace Uset_zayavok.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- Uset_zayavok/Controllers/ListController.cs
+++ Uset_zayavok/Controllers/ListController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 using Uset_zayavok.Models;
@@ -6,6 +7,7 @@ using Uset_zayavok.Models;
 
 namespace Uset_zayavok.Controllers
 {
+    [Authorize]
     public class ListController : Controller
     {
         private readonly PostgresContext _context;
